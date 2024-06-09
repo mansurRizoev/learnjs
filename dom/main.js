aaf7743c8d59599ccd169488bf88a85c6e2e2e8d
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2024-06-09 12:54:31 +0500

    bigi dega humsterbozho

--- dom/main.js
+++ dom/main.js
@@ -1,20 +1,37 @@
-let elH1 = document.createElement('h1');
+// let elH1 = document.createElement('h1');
+/**
+ * document.querySelector
+ * document.querySelectorAll
+ * document.getElementById
+ * document.getElementByTagName
+ */
 let body = document.querySelector('body');
-let name = "Sanjar";
-let age = "19"
-setTimeout(() => {
-    elH1.innerText = "message from setTimeout";
-    body.appendChild(elH1);
-}, 3000)
-setInterval(() => { }, 3000)
+console.log(body)
+// let name = "Sanjar";
+// let age = "19"
+// setTimeout(() => {
+//     elH1.innerText = "message from setTimeout";
+//     body.appendChild(elH1);
+// }, 3000)
+// setInterval(() => { }, 3000)
 
-for (let i = 0; name.length > i; i++) {
+// for (let i = 0; name.length > i; i++) {
 
-    setInterval(() => {
-        let h1 = document.createElement('h1');
-        h1.innerText = name[i]
-        body.appendChild(h1);
-    }, 3000)
+//     setInterval(() => {
+//         let h1 = document.createElement('h1');
+//         h1.innerText = name[i]
+//         body.appendChild(h1);
+//     }, 3000)
 
-}
-
+// }
+let inputRef = document.getElementById('inputRef');
+let inputRef2 = document.getElementById('inputRef2');
+let buttonRef = document.getElementById('btn');
+console.log(inputRef);
+console.log(buttonRef);
+buttonRef.addEventListener('click', () => {
+    let el = document.createElement(inputRef.value);
+    el.innerText = inputRef2.value;
+    body.appendChild(el)
+    console.log(inputRef.value);
+})
