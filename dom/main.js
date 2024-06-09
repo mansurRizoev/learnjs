1d8c006f5302d12836e9ba476ac9d04662734dec
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2024-06-09 12:12:35 +0500

    changed

--- dom/main.js
+++ dom/main.js
@@ -1,13 +1,12 @@
-// h1.textContent = 'Hello World';
 let elH1 = document.createElement('h1');
 let body = document.querySelector('body');
 let name = "Sanjar";
 
-// setTimeout(() => {
-//     elH1.innerText = "message from setTimeout";
-//     body.appendChild(elH1);
-// }, 3000)
-// setInterval(() => { }, 3000)
+setTimeout(() => {
+    elH1.innerText = "message from setTimeout";
+    body.appendChild(elH1);
+}, 3000)
+setInterval(() => { }, 3000)
 
 for (let i = 0; name.length > i; i++) {
 
